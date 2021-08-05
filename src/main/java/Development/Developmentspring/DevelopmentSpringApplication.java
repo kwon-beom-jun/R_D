cc82baa61ba28325486ff7a8863c5e9ded841570
kwon-beom-jun <qjawns0618@naver.com>
2021-08-05 21:56:00 +0900

    Initial commit

---
+++ src/main/java/Development/Developmentspring/DevelopmentSpringApplication.java
@@ -0,0 +1,13 @@
+package Development.Developmentspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Development/Developmentspring/DevelopmentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Development.Developmentspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
